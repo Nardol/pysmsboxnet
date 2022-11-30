0fd4a604ee1a81f0d241ec8282ef35828349487a
Patrick ZAJDA <patrick@zajda.fr>
2022-11-30 17:30:24 +0100

    Comment unused config variables
    Signed-off-by: Patrick ZAJDA <patrick@zajda.fr>

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ extensions = [
     "sphinx.ext.autodoc",
 ]
 
-templates_path = ["_templates"]
+# templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
@@ -37,5 +37,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 html_show_sourcelink = False
