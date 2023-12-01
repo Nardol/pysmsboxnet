566c7093322914806e3195f2cb73d55cfabbdd49
Patrick ZAJDA <patrick@zajda.fr>
2023-12-01 14:45:29 +0100

    Also test on Windows
    Signed-off-by: Patrick ZAJDA <patrick@zajda.fr>

---
+++ script/copy_example.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+
+PROJECT_PATH = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+EXAMPLE_PATH = os.path.join(PROJECT_PATH, "example.py")
+TEMP_DIR = os.getenv("RUNNER_TEMP")
+EXAMPLE_DEST = os.path.join(TEMP_DIR, "test_example")
+
+os.mkdir(EXAMPLE_DEST)
+shutil.copy(EXAMPLE_PATH, EXAMPLE_DEST)
