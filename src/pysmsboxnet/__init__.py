4802778fac33ea9c1431d2a4e60d42db9e60cdea
Patrick ZAJDA <patrick@zajda.fr>
2024-01-11 15:11:15 +0100

    Switch to setuptools-scm
    Signed-off-by: Patrick ZAJDA <patrick@zajda.fr>

--- src/pysmsboxnet/__init__.py
+++ src/pysmsboxnet/__init__.py
@@ -1,1 +1,7 @@
 """Python library to use smsbox.net API."""
+try:
+    from ._version import version as __version__  # type: ignore
+    from ._version import version_tuple  # type: ignore
+except ImportError:
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
