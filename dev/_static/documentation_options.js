b738067a6277aaa00fac32a8d4a2d275ca1c00ee
Nardol <patrick@zajda.fr>
2025-11-10 20:25:24 +0000

    Deploying to gh-pages from @ Nardol/pysmsboxnet@3fd8a7ef1c11acac7e72468670d5617e1b46883a 🚀

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2024.6.2.dev439+g9979a2021',
+    VERSION: '2024.6.2.dev440+g3fd8a7ef1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
